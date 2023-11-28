de1690ac452103f02b8b5a6e05d36ea19581ae16
kartikpaliwal <avinashpaliwal961@gmail.com>
2023-11-28 21:51:04 +0100

    Added ex2

---
+++ exercises/exercise2.py
@@ -0,0 +1,63 @@
+"""
+TASKS: 
+# Build an automated data pipeline for the following source:
+    *direct link to CSV: https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV
+
+# Goals: 
+1. Write data into a SQLite database called “trainstops.sqlite”, in the table “trainstops”
+2. First, drop the "Status" column
+3. Then, drop all rows with invalid values:
+   3.1 Valid "Verkehr" values are "FV", "RV", "nur DPN"
+   3.2 Valid "Laenge", "Breite" values are geographic coordinate system values between and including -90 and 90
+   3.3 Valid "IFOPT" values follow this pattern:
+       *  <exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
+       *  This is not the official IFOPT standard, please follow our guidelines and not the official standard
+   3.4Empty cells are considered invalid
+4. Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
+
+"""
+
+
+import pandas as pd
+import sqlalchemy as sql
+
+class Data_Fetcher:
+    def __init__(self, url, db_name, table_name):
+        self.url = url
+        self.db_name = db_name
+        self.table_name = table_name
+
+    def reader(self):
+        df = pd.read_csv(self.url, delimiter=';') \
+               .drop(columns='Status', errors='ignore') \
+               .loc[lambda x: x['Verkehr'].isin(["FV", "RV", "nur DPN"])] \
+               .loc[lambda x: x['IFOPT'].str.match(r'^.{2}:\d+:\d+(?::\d+)?$').fillna(False)]
+        
+        df['Laenge'] = pd.to_numeric(df['Laenge'].str.replace(',', '.'), errors='coerce')
+        df['Breite'] = pd.to_numeric(df['Breite'].str.replace(',', '.'), errors='coerce')
+        df = df[df['Laenge'].between(-90, 90) & df['Breite'].between(-90, 90)]
+
+        Database_Connection = sql.create_engine(f"sqlite:///{self.db_name}.sqlite", echo=False)
+
+        dtype_mapping = {
+            'NAME': sql.types.TEXT,
+            'Verkehr': sql.types.TEXT,
+            'IFOPT': sql.types.TEXT,
+            'Laenge': sql.types.FLOAT,
+            'Breite': sql.types.FLOAT,
+            'DS100': sql.types.TEXT,
+            'Betreiber_Name': sql.types.TEXT,
+            'Betreiber_Nr': sql.types.BIGINT,
+            'EVA_NR': sql.types.BIGINT,
+        }
+
+        df.to_sql(self.table_name, con=Database_Connection, if_exists="replace", index=False, dtype=dtype_mapping)
+        return df.copy()
+
+if __name__ == '__main__':
+    db_url = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+    db_name = 'trainstops'
+    table_name = 'trainstops'
+
+    pp = Data_Fetcher(db_url, db_name, table_name)
+    df = pp.reader()
